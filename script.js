4f7fbce9b2107b0ca644b48d354596718c68768e
Benson Mwangi <kimaniben@gmail.com>
2024-03-17 10:06:16 +0000

    updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const solutions = [
         problem: "Thrips", 
         infestationimage: "images/Thrips in.jpg",
         description: "Thrips infestations pose a serious threat to coffee production, impacting tree health and berry quality. Vigilance and prompt action are essential for mitigating these pests' impact. By employing early detection methods and implementing appropriate management strategies, coffee farmers can protect their crops and ensure a sustainable yield in the face of thrips infestations.",
-        solution: "Use Kingcode 10mls + Integra 3mls/20ltrs of water",
+        solution: "Use Kingcode 10mls + Integra 3mls/20ltrs of water.",
         image: "images/Kingcode-elite-2048x2048.webp"
        
     },
@@ -12,15 +12,15 @@ const solutions = [
         problem: "Leaf Miners", 
         infestationimage: "images/leafminer.jpg",
         description:"Leaf miners are tiny insect larvae that tunnel through the tissues of leaves, leaving distinctive trails or mines behind them. In the case of coffee plants, leaf miners can have a significant impact on the health and productivity of the coffee leaves. When leaf miners infest coffee plants, they create tunnels within the leaves, which disrupt the flow of nutrients and water.",
-        solution: "Use Presento 10gms + Integra 3mls/20ltrs of water",
+        solution: "Use Presento 10gms + Integra 3mls/20ltrs of water.",
         image: "images/PRESENTO-KG-2048x2048.webp"
     },
    
     { 
         problem: "Coffee Berry Month", 
-        infestationimage: "images/leafminer.jpg",
+        infestationimage: "images/months.jpeg",
         description: "The female moth lays scale-like eggs singly on or near green berries. The caterpillar is reddish to pink in colour with dark markings on the back, and measures 13 mm when fully grown. Caterpillars bore into green, half-grown berries, starting near the stalk and hollow them out.",
-        solution: "Use Kingcode 10mls + Integra 3mls/20ltrs of water",
+        solution: "Use Kingcode 10mls + Integra 3mls/20ltrs of water.",
         image: "images/Kingcode-elite-2048x2048.webp"
     },
 
@@ -28,30 +28,30 @@ const solutions = [
         problem: "Coffee Berry Borers", 
         infestationimage: "images/Coffee berry borer.jpg",
         description: "Coffee berry borer  is a devastating insect pest that affects coffee plants. The female beetle burrows into the coffee cherry and lays her eggs inside. Once hatched, the larvae feed on the coffee beans inside the berry, causing damage and reducing the quality and yield of the coffee crop.",
-        solution: "Use Lexus 10mls + Integra 3mls/20ltrs of water",
+        solution: "Use Lexus 10mls + Integra 3mls/20ltrs of water.",
         image: "images/Lexus-247SC.webp"
     },
     
 
     { 
-        problem: "Mealy Bugs", 
+        problem: "Mealybugs", 
         infestationimage: "images/Mealybugs.jpg",
         description:"Mealybugs are small, white, cotton-like insects that infest coffee plants, sucking sap and causing stunted growth, yellowing leaves, and reduced yield. They also excrete honeydew, attracting ants and promoting sooty mold growth. Control involves physical removal, insecticidal soap, biological agents, or pesticides for severe infestations. ",
-        solution: "Test soil pH and add lime to raise pH levels if soil is too acidic.",
+        solution: "Use Lexus 10mls + Integra 3mls/20ltrs of water.",
         image: "images/Lexus-247SC.webp"
     },
     { 
         problem: "White Headed Borers", 
         infestationimage: "images/Bore.jpg",
         description:"Larvae feed on wood internally, in the main stem and major branches. When mature, adults will leave exit holes 2-4 mm in diameter. Wood dust may be found on ground nearby entrance holes. Other signs of coffee white stem borer include wilting, defoliation, or yellowing of leaves. Tunneling larvae may also cause ridges to develop on the surface of the stem. ",
-        solution: "Use Kingcode 10mls + Integra 3mls/20ltrs of water",
+        solution: "Use Kingcode 10mls + Integra 3mls/20ltrs of water.",
         image: "images/Kingcode-elite-2048x2048.webp"
     },
     { 
         problem: "Mites", 
         infestationimage: "images/Mites.jpg",
         description:"Spider mites cause damage by sucking cell contents from leaves. A small number of mites isn't usually a problem, but very high populations can be damaging, especially to annual plants. Often, damage first appears as a stippling of light dots on the leaves; sometimes leaves turn a bronze color",
-        solution: "Use Bazooka 10mls + Integra 3mls/20ltrs of water",
+        solution: "Use Bazooka 10mls + Integra 3mls/20ltrs of water.",
         image: "images/Bazooka-2048x2048.webp"
     },
     { 
@@ -79,7 +79,7 @@ const solutions = [
         problem: "Zinc Deficiency", 
         infestationimage: "images/Zinc.jpg",
         description:"Zinc deficiency causes leaf borders can become curled and the internodes between the leaves are shortened. The green leaf veins stand out from a yellow background. Symptoms are usually more visible at the tips of the branches in new growth. The tree is stunted and takes on a rosette habit.",
-        solution: "Use Zinc Gold 20mls + Integra 3mls/20ltrs of water especiacly after prunning and during flowering season",
+        solution: "Use Zinc Gold 20mls + Integra 3mls/20ltrs of water especiacly after prunning and during flowering season.",
         image: "images/zinc-gold.webp"
     }, { 
         problem: "Boron Deficiency", 
@@ -113,7 +113,7 @@ const solutions = [
     { 
         problem: "Soil Acidity", 
         description:"Soil acidity is a critical factor affecting plant health and crop productivity. It refers to the pH level of the soil, with acidic soils having pH values below 7. Acidic soils can hinder nutrient availability, impacting plant growth and development.Managing soil acidity is essential for optimizing agricultural yields. Techniques such as liming can help raise soil pH, making essential nutrients more available to plants. Regular soil testing and pH monitoring are vital for maintaining optimal soil conditions and maximizing crop performance.In conclusion, soil acidity significantly influences agricultural outcomes, and proactive management is key to ensuring healthy and productive soil for sustainable farming practices.",
-        solution: "Use Humipower 1kg per 50kg of basal fertilizer",
+        solution: "Use Humipower 1kg per 50kg of basal fertilizer.",
         image: "images/Humipower-bag-2048x2048.webp"
     
 
